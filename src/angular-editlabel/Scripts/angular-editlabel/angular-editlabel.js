d66fa8b5d7025c6f63206af2779c4edb2ee820d8
JeffGos <jeffery.gosling@gmail.com>
2014-12-18 17:14:07 +0000

    - initial commit

---
+++ src/angular-editlabel/Scripts/angular-editlabel/angular-editlabel.js
@@ -0,0 +1,32 @@
+﻿angular.module('angularEditLabel', [])
+
+.directive('editLabel', ['$timeout', function ($timeout) {
+	function link(scope, element, attrs) {
+
+		scope.editing = false;
+
+		scope.onLabelClick = function() {
+			scope.editing = true;
+
+			$timeout(function() {
+				angular.element(element[0].children[1]).focus();
+			});
+		}
+
+		scope.onTextAreaBlur = function () {
+
+			if (scope.ngModel) {
+				scope.editing = false;
+			}
+		}
+	}
+
+	return {
+		restrict: 'E',
+		scope: {
+			ngModel: '='
+		},
+		templateUrl: 'angular-editlabel.html',
+		link: link
+	}
+}]);
